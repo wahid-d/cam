18f5e1752143a2b402f9c37b6aa0a4fa2c73fa86
wahid-d <wakhid2802@gmail.com>
2020-06-08 22:52:53 +0900

    Student Model changed

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -19,17 +20,20 @@ namespace cam.Models
         public string KoreanName { get; set; }
 
         [Required]
+        [StringLength(55, ErrorMessage = "Shool name is too long.")]
+        public string SchoolName { get; set; }
+
+        [Required]
         [StringLength(20)]
         public string Level { get; set; }
 
+        [Required]
         [StringLength(20, ErrorMessage = "Phone number is too long.")]
         public string Phone { get; set; }
 
+        [Required]
         [StringLength(255, ErrorMessage = "Address is too long.")]
         public string Address { get; set; }
-
-        public DateTime Birthdate { get; set; }
-
 
         public string ClassId { get; set; }
         public Class Class { get; set; }
