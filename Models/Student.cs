510030472163d78c28b2e7c3de441f3504716d21
Jin Park <furno5117@gmail.com>
2020-06-09 00:42:14 +0900

    Jins contribution

--- Models/Student.cs
+++ Models/Student.cs
@@ -13,29 +13,39 @@ namespace cam.Models
 
         [Required]
         [StringLength(255, ErrorMessage = "Name is too long.")]
+        [Display(Prompt="Sarah", Name="영어 이름")]
         public string EnglishName { get; set; }
 
         [Required]
         [StringLength(255, ErrorMessage = "Name is too long.")]
+        [Display(Prompt="홍길동", Name="한국어 이름")]
         public string KoreanName { get; set; }
 
         [Required]
-        [StringLength(55, ErrorMessage = "Shool name is too long.")]
+        [StringLength(55, ErrorMessage = "School name is too long.")]
+        [Display(Prompt="신도림 초등학교", Name="학교 이름")]
         public string SchoolName { get; set; }
 
         [Required]
         [StringLength(20)]
+        [Display(Name="레벨")]
         public string Level { get; set; }
 
         [Required]
         [StringLength(20, ErrorMessage = "Phone number is too long.")]
+        [Display(Prompt="010 1234 5678", Name="전화번호")]
         public string Phone { get; set; }
 
         [Required]
         [StringLength(255, ErrorMessage = "Address is too long.")]
+        [Display(Prompt="신도림동 현대 아파트", Name="주소")]
         public string Address { get; set; }
 
+        [Required]
+        [Display(Name="클래스 선택")]        
         public string ClassId { get; set; }
         public Class Class { get; set; }
+
+        
     }
 }
